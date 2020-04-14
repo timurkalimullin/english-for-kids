0f95a4bac37a57a14eff68c39d7fc7744d43460d
Timur Kalimullin <timur_kalimullin@list.ru>
2020-04-14 13:30:37 +0500

    feat: add audio and flip to card

--- index.js
+++ index.js
@@ -31,11 +31,29 @@ class Card {
 
   renderCard() {
     const card = document.createElement('div');
+    const cardAudio = this.data.audioSrc;
     card.classList.add('card');
-    const front = document.createElement('div');
-    front.classList.add('front');
-    front.style.backgroundImage = `url("${this.data.image}")`;
-    card.append(front);
+    card.innerHTML = `
+    <div class="front" style="background-image: url(${this.data.image});">
+    <div class="card-header">${this.data.word}</div></div>
+    <div class="back" style="background-image: url(${this.data.image});">
+    <div class="card-header">${this.data.translation}</div></div>
+    <div class="rotate"></div>
+    `;
+
+    card.addEventListener('click', (e)=>{
+      if (e.target === document.querySelector('.rotate')) {
+        card.classList.add('flip');
+      }
+      if (e.target === document.querySelector('.front')) {
+        let audio = new Audio(cardAudio);
+        audio.play();
+      }
+    });
+
+    card.addEventListener('mouseleave', (e)=>{
+        card.classList.remove('flip');
+    });
 
     return card;
   }
